935c379c3ee28082883f6cded2c29176719b3de0
IsaFarias26 <isa.farias@duocuc.cl>
2024-06-12 09:33:35 -0400

    Front-Djngo-Models-Admin

--- TyF/models.py
+++ TyF/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.utils import timezone
 
 
-class VideoJuegos(models.Model):
+class VideoJuegos(models.Model): (clave primaria)
     Autor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     Titulo = models.CharField(max_length=200)
     Descripción = models.TextField()
