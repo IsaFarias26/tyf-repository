d6090e1c60c729eef4b236eb31e876d2d29228df
Isaí Jeremías <conai2602018@gmail.com>
2024-07-09 12:19:08 -0400

    urls,login

--- TyF/urls.py
+++ TyF/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path, include
-from TyF.views import index
-
+from . import views
 
 urlpatterns = [
-    path('inicio/', index),
+    path('inicio/', views.index, name='inicio'),
+    path('catalogo/', views.catalogo, name='catalogo'),
+    path('contacto/', views.contacto, name='contacto'),
+    path('carrito/', views.carrito, name='carrito'),
+    path('nosotros/', views.nosotros, name='nosotros'),
+    path('producto/', views.producto, name='producto'),
+    path('pruebas/', views.pruebas, name='pruebas'),
+    path('crud/', views.crud, name='crud'),
 ]
--- teclasYfichas/urls.py
+++ teclasYfichas/urls.py
@@ -2,15 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from TyF import views
 
+
 urlpatterns = [
+    path('', views.index, name='inicio'),
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
     path('TyF/', include('TyF.urls')),
-    path('catalogo/', views.catalogo, name='catalogo'),
-    path('contacto/', views.contacto, name='contacto'),
-    path('carrito/', views.carrito, name='carrito'),
-    path('nosotros/', views.nosotros, name='nosotros'),
-    path('producto/', views.producto, name='producto'),
-    path('pruebas/', views.pruebas, name='pruebas'),
-    path('crud/', views.crud, name='crud'),
+    path('account/', include('django.contrib.auth.urls')),
 ]
